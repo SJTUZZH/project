d85b568961c8a6c5311c86150a46f4a432cdfb50
SJTUZZH <40952690+SJTUZZH@users.noreply.github.com>
2018-11-17 22:03:08 +0800

    Add files via upload

--- Hazard_Detection.v
+++ Hazard_Detection.v
@@ -36,27 +36,23 @@ end
 
  //load-use hazard detection
 always@(*) begin
-   if((MemRead_EX==1'b1)&&( (rt_EX==rs_ID)||(rt_EX==rt_ID) )) begin
-        nop=1'b1;
+if(Branch==1'b1) begin 
+    IFID_flush=1'b1;
+    if (((RegWrite_EX==1'b1)&&(rd_EX)&&((rd_EX==rs_ID)||(rd_EX==rt_ID)))||
+    (((MemRead_MEM==1'b1)||(MemRead_EX==1'b1))&&(rd_MEM)&&((rd_MEM==rs_ID)||(rd_MEM==rt_ID))))begin//1st preceding ALU or 2nd preceding load or 1str precding load
+        nop=1'b1;IDEX_flush=1'b1;
     end
-    else begin nop=1'b0; end
-end
-
-
-//branch hazard detection
-always@(*) begin
-   if(Branch==1'b1) begin
-        nop=1'b1;
-        IFID_flush=1'b1;
-        if (((RegWrite_EX==1'b1)&&(rd_EX)&&((rd_EX==rs_ID)||(rd_EX==rt_ID)))
-        ||(((MemRead_MEM==1'b1)||(MemRead_EX==1'b1))&&(rd_MEM)&&((rd_MEM==rs_ID)||(rd_MEM==rt_ID))))begin//1st preceding ALU or 2nd preceding load or 1str precding load
-            IDEX_flush=1'b0;
-        end
-        else begin IDEX_flush=1'b0; end
+    else begin nop=1'b0;IDEX_flush=1'b0;end
+ end
+ 
+ else begin
+    IFID_flush=1'b0;
+    if((MemRead_EX==1'b1)&&( (rt_EX==rs_ID)||(rt_EX==rt_ID) )) begin
+        nop=1'b1;IDEX_flush=1'b1;
     end
-    else begin IFID_flush=1'b0; end
-end
-
+    else begin nop=1'b0;IDEX_flush=1'b0;end
+ end
+ end
 
 
 endmodule
